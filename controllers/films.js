cbbade2bd37ee45667b708234c7bf06e1f8ef283
Kelsie Braidwood <kelsieiona@gmail.com>
2018-06-18 19:59:13 +0100

    adds route for getting landing page of welcome

--- controllers/films.js
+++ controllers/films.js
@@ -1,3 +1,7 @@
 const films = require('../models/films')();
 const Film = require('../models/film');
 const Review = require('../models/review');
+const express = require('express');
+const filmRouter = new express.Router();
+
+module.exports = filmRouter;
