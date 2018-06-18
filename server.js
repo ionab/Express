cdbc5843f207a00a60dc20dcf7684f65ea839683
Kelsie Braidwood <kelsieiona@gmail.com>
2018-06-18 19:56:40 +0100

    intital commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(require('./controllers/index.js'))
+app.use(require('./controllers/films.js'))
+
+
+app.listen(3000, function () {
+  console.log('App running on port '+this.address().port);
+});
